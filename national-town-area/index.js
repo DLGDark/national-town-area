26b9923869fe46a320974eb565a555494961adbc
dlg <1399477141.qq.com>
2019-03-26 14:11:25 +0800

    town

---
+++ national-town-area/index.js
@@ -0,0 +1,11 @@
+/**
+ * Export TownshipArea (with )
+ */
+
+exports = module.exports = require('./lib/townshipArea.js');
+
+/*
+  Export the version
+*/
+
+exports.version = require('./package.json').version;
