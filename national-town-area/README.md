1559525be7b778570ddb4ec8bd78f11c394d0645
dlg <1399477141.qq.com>
2019-11-08 19:43:50 +0800

    update

--- national-town-area/README.md
+++ national-town-area/README.md
@@ -1,6 +1,6 @@
 
 **调用方法名：**
- - TownshipArea
+ - TownshipArea(Promise异步调用，内部代码已将执行结果以JSON格式写入文件，并返回)
 
 **参数：**
 
@@ -8,4 +8,26 @@
  - 类型：String or Number
  - 值：区/县code码值(6位或12位)
 
+  **返回值：**
+
+ - 该县/区下的所有乡镇(json格式)
+
 **结果：** 根据传入的区/县code码值获取到的对应的乡镇的区域code码值及乡/镇名称并写入json文件
+
+**代码中使用方式:**
+```javascript
+// 文件头部require方式引入
+const area = require('national-town-area');
+
+// 代码中使用
+area.TownshipArea()
+        .then(res => {
+          // 这里写拿到数据后的自己的一些操作代码
+          // 因为内部是异步调用方式，这里用then来接受结果，
+          // 结果为查找到的该县/区下的所有乡镇数据，数据格式为json格式
+          console.log(res);
+        })
+        .catch(err => { 
+          console.log(err);
+        });
+```
--- national-town-area/lib/townshipArea.js
+++ national-town-area/lib/townshipArea.js
@@ -7,103 +7,108 @@ const http = require('http');
 //引入编码格式处理模块
 const iconv = require('iconv-lite');
 // 引入文件系统模块
-const fs=require('fs');
+const fs = require('fs');
 
 // url默认前缀
 var staticUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/";
 // 获取当前年份值
-var year = new Date().getFullYear() -1;
+var year = new Date().getFullYear() - 1;
 //存储结果值
 var onlyObj = {};
 
 
 function TownshipArea(countyCode) {
 
-	if(countyCode === '' || countyCode === undefined) return ;
+  if (countyCode === '' || countyCode === undefined) return;
 
-    if(typeof countyCode !== "number" && typeof countyCode !== "string" ){
-        try {
-            throw new TypeError('Incoming parameter type error', "index.js");
-        } catch (e) {
-            console.log(e.name+':'+e.message+' in '+e.fileName);
-            console.log(e.stack);
-        }
+  if (typeof countyCode !== "number" && typeof countyCode !== "string") {
+    try {
+      throw new TypeError('Incoming parameter type error', "index.js");
+    } catch (e) {
+      console.log(e.name + ':' + e.message + ' in ' + e.fileName);
+      console.log(e.stack);
     }
+  }
 
-    let _countyCode = typeof countyCode === "number" ? String(countyCode) : countyCode ;
+  let _countyCode = typeof countyCode === "number" ? String(countyCode) : countyCode;
 
-    let provinceCode = _countyCode.substr(0, 2);
-    let cityCode = _countyCode.substr(2, 2);
+  let provinceCode = _countyCode.substr(0, 2);
+  let cityCode = _countyCode.substr(2, 2);
 
-    // 拼接出最终正确的url
-    let url = staticUrl + year + '/' + provinceCode + '/' + cityCode + '/' + _countyCode.substr(0, 6) + '.html';
+  // 拼接出最终正确的url
+  let url = staticUrl + year + '/' + provinceCode + '/' + cityCode + '/' + _countyCode.substr(0, 6) + '.html';
 
-    // 发起请求
+  // 发起请求
+  const resultPromise = new Promise((resolve, reject) => {
     http.get(url, (res) => {
-        let chunkBuffers = [];
+      let chunkBuffers = [];
 
-        res.on('data', (chunk) => {
-            chunkBuffers.push(chunk);
-        });
+      res.on('data', (chunk) => {
+        chunkBuffers.push(chunk);
+      });
 
-        res.on('end', () => {
-            try {
-                // 对字节进行处理
-                let buffer = Buffer.concat(chunkBuffers);
-                // 对window环境下的返回字符编码为gb2312编码格式的字符内容进行处理
-                let html = iconv.decode(buffer, 'gb2312');
+      res.on('end', () => {
+        try {
+          // 对字节进行处理
+          let buffer = Buffer.concat(chunkBuffers);
+          // 对window环境下的返回字符编码为gb2312编码格式的字符内容进行处理
+          let html = iconv.decode(buffer, 'gb2312');
 
-                let $townCheerio = cheerio.load(html);
+          let $townCheerio = cheerio.load(html);
 
-                let $town = $townCheerio('.towntr');
-                let townObj = [];
+          let $town = $townCheerio('.towntr');
+          let townObj = [];
 
-                let $td = '';
+          let $td = '';
 
-                $town.each(function (i, item) {
-                    $td = $townCheerio(item).find('td');
-                    let code = '';
-                    let name = '';
+          $town.each(function (i, item) {
+            $td = $townCheerio(item).find('td');
+            let code = '';
+            let name = '';
 
-                    $td.each(function (i, item) {
-                        if ($townCheerio(item).find('a').length > 0) {
-                            if (i === 0) {
-                                code = $townCheerio(item).find('a').text();
-                            } else {
-                                name = $townCheerio(item).find('a').text();
-                            }
-                        } else {
-                            if (i === 0) {
-                                code = $townCheerio(item).text();
-                            } else {
-                                name = $townCheerio(item).text();
-                            }
-                        }
-                    });
+            $td.each(function (i, item) {
+              if ($townCheerio(item).find('a').length > 0) {
+                if (i === 0) {
+                  code = $townCheerio(item).find('a').text();
+                } else {
+                  name = $townCheerio(item).find('a').text();
+                }
+              } else {
+                if (i === 0) {
+                  code = $townCheerio(item).text();
+                } else {
+                  name = $townCheerio(item).text();
+                }
+              }
+            });
 
-                    townObj.push({
-                        townCode: code,
-                        townName: name
-                    });
-                });
+            townObj.push({
+              townCode: code,
+              townName: name
+            });
+          });
 
-                onlyObj["countyCode"] = _countyCode;
-                onlyObj.town = copy.deepClone(townObj);
+          onlyObj["countyCode"] = _countyCode;
+          onlyObj.town = copy.deepClone(townObj);
 
-                // 将数据写入文件
-                fs.writeFile(year+'乡镇地区数据.json', JSON.stringify(onlyObj), (err) => {
-                    if (err) throw err;
-                    console.log('Township area data was successfully written');
-                });
+          // 将数据写入文件
+          fs.writeFile(year + '乡镇地区数据.json', JSON.stringify(onlyObj), (err) => {
+            if (err) throw err;
+            console.log('Township area data was successfully written');
+            resolve(JSON.stringify(onlyObj));
+          });
 
-            } catch (e) {
-                console.error(e.message);
-            }
+        } catch (e) {
+          console.error(e.message);
+        }
 
-        }).on('error', (e) => {
-            console.error(`出现错误: ${e.message}`);
-        });
+      }).on('error', (e) => {
+        console.error(`出现错误: ${e.message}`);
+      });
     });
+  });
+
+  return resultPromise;
 }
 
-module.exports = {TownshipArea}
+module.exports = { TownshipArea }
